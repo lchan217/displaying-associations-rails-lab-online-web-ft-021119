f1a72e09e9ce2597899989777b1db553f5d3ebe8
Lizzie <lchan217@gmail.com>
2019-04-24 16:32:54 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,9 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+  def formatted
+    "#{self.artist.name} - #{self.title}"
+  end
+  def artist_name
+    self.artist.name
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :songs, only: [:show, :index, :new]
+  resources :artists, only: [:show, :index, :new]
 end
